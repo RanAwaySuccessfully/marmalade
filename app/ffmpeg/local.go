02d09d871d0b23598eba6a0b5ce5f3bc207f9a8a
RanAS <RanAwaySuccessfully@users.noreply.github.com>
2026-07-06 17:23:37 -0300

    change local ffmpeg CFLAGS and LDFLAGS

--- app/ffmpeg/local.go
+++ app/ffmpeg/local.go
@@ -3,7 +3,7 @@
 package main
 
 /*
-#cgo CFLAGS: -I${SRCDIR}/ffmpeg/libavcodec -I${SRCDIR}/ffmpeg/libavutil -I${SRCDIR}/ffmpeg/libswscale
-#cgo LDFLAGS: ${SRCDIR}/ffmpeg/libavcodec/libavcodec.so ${SRCDIR}/ffmpeg/libavutil/libavutil.so ${SRCDIR}/ffmpeg/libswscale/libswscale.so -lavcodec -lavutil -lswscale
+#cgo CFLAGS: -I${SRCDIR}/ffmpeg
+#cgo LDFLAGS: -L${SRCDIR}/ffmpeg/libavcodec -L${SRCDIR}/ffmpeg/libavutil -L${SRCDIR}/ffmpeg/libswscale -lavcodec -lavutil -lswscale
 */
 import "C"
